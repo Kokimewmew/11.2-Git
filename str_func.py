c5f556247f37f4333a232842cd54271c48af0b10
Artur <kokivirivoki@mail.ru>
2024-02-06 15:18:16 +0500

    Add docstring for str_func, Add str_func_capitalize

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_func(str_word):
+    """Функция поднимает строку в верхний регистр"""
     return str_word.upper()
+
+
+def str_func_capitalize(str_word):
+    """Функция поднимает первую букву строки в верхний регистр"""
+    return str_word.capitalize()
